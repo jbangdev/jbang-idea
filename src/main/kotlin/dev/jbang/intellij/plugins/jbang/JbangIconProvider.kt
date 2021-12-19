63b9b6dfc114c3a1dd7099a182b831145f32fd4a
linux_china <libing.chen@gmail.com>
2021-12-19 15:29:24 -0800

    Use jbangIcon

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/JbangIconProvider.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/JbangIconProvider.kt
@@ -2,12 +2,10 @@ package dev.jbang.intellij.plugins.jbang
 
 import com.intellij.ide.FileIconProvider
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.vfs.VirtualFile
 import javax.swing.Icon
 
 class JbangIconProvider : FileIconProvider {
-    private val jbangIcon = IconLoader.findIcon("jbang-16x16.png")
 
     override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? {
         return if (file.name == "jbang-catalog.json") {
