b927f35a51ca9ec72a80a55861f66785840bd74f
linux_china <libing.chen@gmail.com>
2021-12-19 15:31:05 -0800

    JbangConfigurationType

---
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/run/JbangConfigurationType.kt
@@ -0,0 +1,34 @@
+package dev.jbang.intellij.plugins.jbang.run
+
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.configurations.ConfigurationType
+import dev.jbang.intellij.plugins.jbang.jbangIcon
+import javax.swing.Icon
+
+class JbangConfigurationType : ConfigurationType {
+    companion object {
+        const val ID = "JBangRunConfiguration"
+    }
+
+    override fun getDisplayName(): String {
+        return "JBang"
+    }
+
+    override fun getConfigurationTypeDescription(): String {
+        return "JBang run configuration type"
+    }
+
+    override fun getIcon(): Icon {
+        return jbangIcon
+    }
+
+    override fun getId(): String {
+        return ID
+    }
+
+    override fun getConfigurationFactories(): Array<ConfigurationFactory> {
+        return arrayOf(JbangConfigurationFactory(this))
+    }
+
+
+}
