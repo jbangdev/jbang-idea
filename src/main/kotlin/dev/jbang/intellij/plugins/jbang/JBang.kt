88f90139eaa1676020d9b059986b274a16246501
linux_china <libing.chen@gmail.com>
2021-12-20 13:06:38 -0800

    Add icons

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBang.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBang.kt
@@ -3,4 +3,7 @@ package dev.jbang.intellij.plugins.jbang
 import com.intellij.openapi.util.IconLoader
 
 
-val jbangIcon = IconLoader.findIcon("jbang-16x16.png")!!
+val jbangIcon = IconLoader.findIcon("icons/jbang-16x16.png")!!
+val kotlinIcon = IconLoader.findIcon("icons/kotlin.svg")!!
+val groovyIcon = IconLoader.findIcon("icons/groovy.svg")!!
+val jshellIcon = IconLoader.findIcon("icons/jshell-16x16.png")!!
