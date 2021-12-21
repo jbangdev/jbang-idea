660e48c9969cbc5c5a4e11e1811f763420cea602
linux_china <libing.chen@gmail.com>
2021-12-21 14:25:25 -0800

    JbangKotlinCompletionContributor

---
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/completion/JbangKotlinCompletionContributor.kt
@@ -0,0 +1,41 @@
+package dev.jbang.intellij.plugins.jbang.completion
+
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProvider
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiFile
+import com.intellij.util.ProcessingContext
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtNamedFunction
+
+class JbangKotlinCompletionContributor : JbangBaseDirectiveCompletionContributor() {
+    init {
+        extend(
+            CompletionType.BASIC, PlatformPatterns.psiElement(PsiComment::class.java).withLanguage(KotlinLanguage.INSTANCE),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    val comment = parameters.position as PsiComment
+                    val commentParent = comment.parent
+                    if (commentParent is KtNamedFunction || commentParent is KtClass || commentParent is PsiFile) {
+                        if (comment.text.startsWith("//")) {
+                            JAVA_DIRECTIVES.forEach {
+                                result.addElement(LookupElementBuilder.create(it))
+                            }
+                            result.addElement(LookupElementBuilder.create("KOTLIN"))
+                        }
+                    }
+                }
+            }
+        )
+    }
+
+}  
