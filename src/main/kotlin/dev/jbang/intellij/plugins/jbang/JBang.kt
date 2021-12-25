41ab67bbf7a449c71bb811a082abf4d768e2a2fe
linux_china <libing.chen@gmail.com>
2021-12-25 12:08:01 -0800

    Add isJbangScriptFile() & isJbangScript()

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBang.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBang.kt
@@ -7,3 +7,14 @@ val jbangIcon = IconLoader.findIcon("icons/jbang-16x16.png")!!
 val kotlinIcon = IconLoader.findIcon("icons/kotlin.svg")!!
 val groovyIcon = IconLoader.findIcon("icons/groovy.svg")!!
 val jshellIcon = IconLoader.findIcon("icons/jshell-16x16.png")!!
+
+fun isJbangScriptFile(fileName: String): Boolean {
+    return fileName.endsWith(".java")
+            || fileName.endsWith(".kt")
+            || fileName.endsWith(".jsh")
+            || fileName.endsWith(".groovy")
+}
+
+fun isJbangScript(code: String): Boolean {
+    return code.contains("///usr/bin/env jbang") || code.lines().any { it.startsWith("//DEPS") };
+}
