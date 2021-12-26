6cf5edeb655de609d28d69ff03b60ffbcb76ef09
linux_china <libing.chen@gmail.com>
2021-12-25 23:28:28 -0800

    Add some constants

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBang.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBang.kt
@@ -8,21 +8,25 @@ val kotlinIcon = IconLoader.findIcon("icons/kotlin.svg")!!
 val groovyIcon = IconLoader.findIcon("icons/groovy.svg")!!
 val jshellIcon = IconLoader.findIcon("icons/jshell-16x16.png")!!
 
+const val JBANG_DECLARE = "///usr/bin/env jbang"
 val ALL_DIRECTIVES = listOf("JAVA", "DEPS", "GAV", "FILES", "SOURCES", "DESCRIPTION", "REPOS", "JAVAC_OPTIONS", "JAVA_OPTIONS", "JAVAAGENT", "CDS", "KOTLIN", "GROOVY")
+val ALL_EXT_NAMES = listOf(".java", ".kt", ".jsh", ".groovy")
 
 fun isJbangScriptFile(fileName: String): Boolean {
-    return fileName.endsWith(".java")
-            || fileName.endsWith(".kt")
-            || fileName.endsWith(".jsh")
-            || fileName.endsWith(".groovy")
+    val extName = if (fileName.contains('.')) {
+        fileName.substring(fileName.lastIndexOf('.'))
+    } else {
+        ""
+    }
+    return ALL_EXT_NAMES.contains(extName)
 }
 
 fun isJbangScript(code: String): Boolean {
-    return code.contains("///usr/bin/env jbang") || code.lines().any { it.startsWith("//DEPS") };
+    return code.contains(JBANG_DECLARE) || code.lines().any { it.startsWith("//DEPS") };
 }
 
 fun isJBangDirective(line: String): Boolean {
-    if (line.startsWith("///usr/bin/env jbang")) return true
+    if (line.startsWith(JBANG_DECLARE)) return true
     if (line.startsWith("//")) {
         var directive = line.substring(2)
         if (directive.contains(' ')) {
