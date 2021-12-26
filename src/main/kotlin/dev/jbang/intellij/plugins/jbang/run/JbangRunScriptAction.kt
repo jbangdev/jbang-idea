c21e21fb17d21e2a89cd36660f200b3524c11a64
linux_china <libing.chen@gmail.com>
2021-12-25 23:24:15 -0800

    JbangRunScriptAction

---
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/run/JbangRunScriptAction.kt
@@ -0,0 +1,23 @@
+package dev.jbang.intellij.plugins.jbang.run
+
+import com.intellij.execution.Executor
+import com.intellij.execution.Location
+import com.intellij.execution.PsiLocation
+import com.intellij.execution.RunManagerEx
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.runners.ExecutionUtil
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import com.intellij.psi.PsiElement
+
+class JbangRunScriptAction(private val target: PsiElement) : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        val dataContext = SimpleDataContext.getSimpleContext(Location.DATA_KEY, PsiLocation(target), event.dataContext)
+        val context = ConfigurationContext.getFromContext(dataContext, event.place)
+        val producer = JbangRunConfigurationProducer()
+        val configuration = producer.findOrCreateConfigurationFromContext(context)?.configurationSettings ?: return
+        (context.runManager as RunManagerEx).setTemporaryConfiguration(configuration)
+        ExecutionUtil.runConfiguration(configuration, Executor.EXECUTOR_EXTENSION_NAME.extensionList.first())
+    }
+}
