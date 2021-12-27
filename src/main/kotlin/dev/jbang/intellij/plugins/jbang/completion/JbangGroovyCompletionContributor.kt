36ef2a8d77aa8662819a1551e04846f83fb392f6
linux_china <libing.chen@gmail.com>
2021-12-26 18:38:33 -0800

    Polishing

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/completion/JbangGroovyCompletionContributor.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/completion/JbangGroovyCompletionContributor.kt
@@ -5,16 +5,16 @@ import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.lang.java.JShellLanguage
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiFile
 import com.intellij.util.ProcessingContext
+import org.jetbrains.plugins.groovy.GroovyLanguage
 
 class JbangGroovyCompletionContributor : JbangBaseDirectiveCompletionContributor() {
     init {
         extend(
-            CompletionType.BASIC, PlatformPatterns.psiElement(PsiComment::class.java).withLanguage(JShellLanguage.INSTANCE),
+            CompletionType.BASIC, PlatformPatterns.psiElement(PsiComment::class.java).withLanguage(GroovyLanguage),
             object : CompletionProvider<CompletionParameters>() {
                 override fun addCompletions(
                     parameters: CompletionParameters,
