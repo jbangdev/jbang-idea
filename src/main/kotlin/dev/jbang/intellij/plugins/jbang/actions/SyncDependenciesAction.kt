533cfe824f2425772b43859d76c5dadd51045d4b
linux_china <libing.chen@gmail.com>
2021-12-26 17:41:05 -0800

    Fix jbangCmd absolute path bug

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/actions/SyncDependenciesAction.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/actions/SyncDependenciesAction.kt
@@ -12,11 +12,11 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ModuleRootModificationUtil
 import com.intellij.openapi.roots.impl.OrderEntryUtil
-import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import dev.jbang.intellij.plugins.jbang.JBANG_DECLARE
+import dev.jbang.intellij.plugins.jbang.getJBangCmdAbsolutionPath
 import dev.jbang.intellij.plugins.jbang.isJbangScript
 import dev.jbang.intellij.plugins.jbang.isJbangScriptFile
 import org.jetbrains.kotlin.idea.core.util.toPsiFile
@@ -24,7 +24,6 @@ import org.jetbrains.kotlin.idea.util.module
 import org.jetbrains.kotlin.idea.util.projectStructure.getModuleDir
 import org.jetbrains.plugins.gradle.util.GradleConstants
 import java.io.BufferedReader
-import java.io.File
 
 /**
  * Sync dependencies between JBang script and Gradle dependencies
@@ -203,12 +202,7 @@ class SyncDependenciesAction : AnAction() {
         ApplicationManager.getApplication().runWriteAction {
             // get new dependencies from `jbang info classpath --quiet script_path`
             val fullPath = jbangScriptFile.virtualFile.path
-            val userHome = System.getProperty("user.home")
-            var jbangCmd = "jbang"
-            if (SystemInfo.isWindows) {
-                jbangCmd += ".cmd"
-            }
-            jbangCmd = File(userHome, ".jbang/bin/${jbangCmd}").absolutePath
+            val jbangCmd = getJBangCmdAbsolutionPath()
             val pb = ProcessBuilder(jbangCmd, "info", "classpath", "--quiet", fullPath)
             val process = pb.start()
             val allText = process.inputStream.bufferedReader().use(BufferedReader::readText).trim()
