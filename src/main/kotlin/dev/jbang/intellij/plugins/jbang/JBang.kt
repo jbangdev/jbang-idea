d20ab8bc92d3fabaaabef9ff95ef0504440c02f8
linux_china <libing.chen@gmail.com>
2021-12-27 18:29:20 -0800

    Add getJBangCmdAbsolutionPath() back

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBang.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBang.kt
@@ -1,6 +1,8 @@
 package dev.jbang.intellij.plugins.jbang
 
 import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.util.SystemInfo
+import java.io.File
 
 val jbangIcon = IconLoader.findIcon("icons/jbang-16x16.png")!!
 val kotlinIcon = IconLoader.findIcon("icons/kotlin.svg")!!
@@ -10,6 +12,19 @@ val jshellIcon = IconLoader.findIcon("icons/jshell-16x16.png")!!
 const val JBANG_DECLARE = "///usr/bin/env jbang"
 val ALL_DIRECTIVES = listOf("JAVA", "DEPS", "GAV", "FILES", "SOURCES", "DESCRIPTION", "REPOS", "JAVAC_OPTIONS", "JAVA_OPTIONS", "JAVAAGENT", "CDS", "KOTLIN", "GROOVY")
 val ALL_EXT_NAMES = listOf(".java", ".kt", ".jsh", ".groovy")
+
+fun getJBangCmdAbsolutionPath(): String {
+    val userHome = System.getProperty("user.home")
+    return if (SystemInfo.isWindows) {
+        File(userHome, ".jbang/bin/jbang.cmd").absolutePath
+    } else {
+        if (File("/usr/local/bin/jbang").exists()) {
+            "/usr/local/bin/jbang"
+        } else {
+            File(userHome, ".jbang/bin/jbang").absolutePath
+        }
+    }
+}
 
 fun isJbangScriptFile(fileName: String): Boolean {
     val extName = if (fileName.contains('.')) {
