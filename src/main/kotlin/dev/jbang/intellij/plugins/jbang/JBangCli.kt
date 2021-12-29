c494e313bea3ac0b25dec9b9f7793ca2a5ade497
linux_china <libing.chen@gmail.com>
2021-12-28 16:56:53 -0800

    Move logic to CLI invoke

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBangCli.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/JBangCli.kt
@@ -1,32 +1,54 @@
 package dev.jbang.intellij.plugins.jbang
 
-import dev.jbang.cli.BaseScriptCommand
-import dev.jbang.cli.Info
-import dev.jbang.cli.Init
-import dev.jbang.util.Util
-import org.apache.commons.lang3.reflect.FieldUtils
-import org.apache.commons.lang3.reflect.MethodUtils
-import picocli.CommandLine
-import kotlin.io.path.Path
+import java.io.BufferedReader
+import java.io.File
 
 object JBangCli {
 
+    private val colorCleanRegex = "\u001B\\[[;\\d]*m".toRegex()
+
+    @Throws(Exception::class)
+    fun listJbangTemplates(): List<String> {
+        val jbangCmd = getJBangCmdAbsolutionPath()
+        val pb = ProcessBuilder(jbangCmd, "template", "list")
+        val process = pb.start()
+        process.waitFor()
+        if (process.exitValue() != 0) {
+            throw Exception(processErrorToText(process))
+        } else {
+            return processOutputToText(process).lines().map {
+                it.replace(colorCleanRegex, "").toString()
+            }
+        }
+    }
+
     @Throws(Exception::class)
     fun resolveScriptDependencies(scriptFilePath: String): List<String> {
-        val args = arrayOf("classpath", "--fresh", scriptFilePath)
-        val demo = Info()
-        val commandLine = CommandLine(demo)
-        commandLine.execute(*args)
-        val baseScriptCommand = commandLine.subcommands["classpath"]!!.getCommand<BaseScriptCommand>()
-        val scriptInfo = MethodUtils.invokeMethod(baseScriptCommand, true, "getInfo")
-        return FieldUtils.readDeclaredField(scriptInfo, "resolvedDependencies", true) as List<String>
+        val jbangCmd = getJBangCmdAbsolutionPath()
+        val pb = ProcessBuilder(jbangCmd, "info", "classpath", "--fresh", scriptFilePath)
+        val process = pb.start()
+        process.waitFor()
+        if (process.exitValue() != 0) {
+            throw Exception(processErrorToText(process))
+        } else {
+            return processOutputToText(process).split(':', ';').filter { !it.contains(".jbang") }
+        }
     }
 
     @Throws(Exception::class)
     fun generateScriptFrommTemplate(templateName: String, scriptName: String, destDir: String) {
-        val args = arrayOf("--template", templateName, "--force", scriptName)
-        Util.setCwd(Path(destDir))
-        val commandLine = CommandLine(Init())
-        commandLine.execute(*args)
+        val jbangCmd = getJBangCmdAbsolutionPath()
+        val filePath = File(destDir, scriptName).absolutePath
+        val pb = ProcessBuilder(jbangCmd, "init", "--template", templateName, "--force", filePath)
+        val process = pb.start()
+        process.waitFor()
+    }
+
+    private fun processOutputToText(process: Process): String {
+        return process.inputStream.bufferedReader().use(BufferedReader::readText).trim()
+    }
+
+    private fun processErrorToText(process: Process): String {
+        return process.errorStream.bufferedReader().use(BufferedReader::readText).trim()
     }
 }
