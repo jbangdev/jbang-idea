4e48c96e5b00cda3f491afd588b0c8ccc775f90e
linux_china <libing.chen@gmail.com>
2021-12-28 17:34:10 -0800

    Copy models from JBang

--- src/main/kotlin/dev/jbang/intellij/plugins/jbang/models.kt
+++ src/main/kotlin/dev/jbang/intellij/plugins/jbang/models.kt
@@ -1,11 +1,31 @@
 package dev.jbang.intellij.plugins.jbang
 
-data class ScriptInfo(
-    val originalResource: String,
-    val backingResource: String,
-    val applicationJar: String,
-    val dependencies: List<String>,
-    val resolvedDependencies: List<String>,
-    val requestedJavaVersion: String,
-    val availableJdkPath: String,
-)
+class ScriptInfo {
+    var originalResource: String? = null
+    var backingResource: String? = null
+    var applicationJar: String? = null
+    var mainClass: String? = null
+    var dependencies: List<String>? = null
+    var repositories: List<Repo>? = null
+    var resolvedDependencies: List<String>? = null
+    var javaVersion: String? = null
+    var requestedJavaVersion: String? = null
+    var availableJdkPath: String? = null
+    var compileOptions: List<String>? = null
+    var runtimeOptions: List<String>? = null
+    var files: List<ResourceFile>? = null
+    var sources: List<ScriptInfo>? = null
+    var description: String? = null
+    var gav: String? = null
+}
+
+class Repo {
+    var id: String? = null
+    var url: String? = null
+}
+
+class ResourceFile {
+    var originalResource: String? = null
+    var backingResource: String? = null
+    var target: String? = null
+}
