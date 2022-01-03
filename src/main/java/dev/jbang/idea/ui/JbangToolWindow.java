65cdca8dfb6e95b0dd277cd70f9bba7c71580d55
linux_china <libing.chen@gmail.com>
2022-01-03 14:11:39 -0800

    JbangToolWindow Java Source

---
+++ src/main/java/dev/jbang/idea/ui/JbangToolWindow.java
@@ -0,0 +1,31 @@
+package dev.jbang.idea.ui;
+
+import dev.jbang.idea.ScriptInfo;
+
+import javax.swing.*;
+
+public class JbangToolWindow {
+    private JPanel toolWindowContent;
+    private JTextField originalResourceField;
+    private JTextField javaField;
+    private JTextField applicationJarField;
+    private JTextArea dependenciesTextArea;
+
+
+    public JPanel getContent() {
+        return toolWindowContent;
+    }
+
+    public void update(ScriptInfo scriptInfo) {
+        if (scriptInfo.getOriginalResource() != null) {
+            final String[] parts = scriptInfo.getOriginalResource().split("[/\\\\]");
+            originalResourceField.setText(parts[parts.length - 1]);
+        }
+        javaField.setText(scriptInfo.getJavaVersion() != null ? scriptInfo.getJavaVersion() : scriptInfo.getRequestedJavaVersion());
+        applicationJarField.setText(scriptInfo.getApplicationJar());
+        if (scriptInfo.getDependencies() != null) {
+            dependenciesTextArea.setText(String.join("\n", scriptInfo.getDependencies()));
+        }
+    }
+}
+
