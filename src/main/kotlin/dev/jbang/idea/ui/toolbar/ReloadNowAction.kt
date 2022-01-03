1c92e1053db2a6cad3c8e6cb22ab95cd8d682958
linux_china <libing.chen@gmail.com>
2022-01-03 15:26:44 -0800

    Introduce BaseToolbarAction

--- src/main/kotlin/dev/jbang/idea/ui/toolbar/ReloadNowAction.kt
+++ src/main/kotlin/dev/jbang/idea/ui/toolbar/ReloadNowAction.kt
@@ -1,19 +1,13 @@
 package dev.jbang.idea.ui.toolbar
 
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.wm.ToolWindowManager
-import dev.jbang.idea.ui.JBangToolWindowPanel
 
-class ReloadNowAction : AnAction() {
+class ReloadNowAction : BaseToolbarAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project!!
-        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("JBang")
-        if (toolWindow != null) {
-            val content = toolWindow.contentManager.contents.first { it.component is JBangToolWindowPanel }
-            val jBangToolWindowPanel = content.component as JBangToolWindowPanel
-
+        val jBangToolWindowPanel = getJBangToolWindowPanel(project)
+        if (jBangToolWindowPanel != null) {
             val fileEditorManager = FileEditorManager.getInstance(project)
             if (fileEditorManager.selectedFiles.isNotEmpty()) {
                 jBangToolWindowPanel.refreshScriptInfo(fileEditorManager.selectedFiles[0])
