91c586d3860b37deb972982d410d96203ada4307
linux_china <libing.chen@gmail.com>
2022-01-17 20:54:50 -0800

    Add JAVA_VERSION, KOTLIN_VERSION, GROOVY_VERSION support

--- src/main/kotlin/dev/jbang/idea/module/JBangModuleBuilder.kt
+++ src/main/kotlin/dev/jbang/idea/module/JBangModuleBuilder.kt
@@ -1,16 +1,20 @@
 package dev.jbang.idea.module
 
-import com.intellij.ide.actions.CreateFileFromTemplateAction.createFileFromTemplate
 import com.intellij.ide.fileTemplates.FileTemplateManager
+import com.intellij.ide.fileTemplates.FileTemplateUtil
 import com.intellij.ide.util.projectWizard.JavaModuleBuilder
 import com.intellij.ide.util.projectWizard.ModuleBuilderListener
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.util.SystemInfo
 import dev.jbang.idea.jbangIcon
 import org.jetbrains.kotlin.idea.core.util.toPsiDirectory
+import org.jetbrains.kotlin.idea.util.projectStructure.sdk
 import java.io.File
+import java.util.*
+import java.util.regex.Pattern
 import javax.swing.Icon
 
 
@@ -34,29 +38,55 @@ class JBangModuleBuilder : JavaModuleBuilder(), ModuleBuilderListener {
 
     override fun moduleCreated(module: Module) {
         val moduleRootManager = ModuleRootManager.getInstance(module)
-        val groovyIncluded = moduleRootManager.orderEntries().allLibrariesAndSdkClassesRoots.any {
+        val properties = Properties()
+        properties.putAll(FileTemplateManager.getInstance(module.project).defaultProperties)
+        val javaSdkVersion = module.sdk?.versionString
+        val javaVersion = if (javaSdkVersion != null) {
+            (9..19).firstOrNull { javaSdkVersion.contains("${it}.0") } ?: 8
+        } else {
+            11
+        }
+        properties["JAVA_VERSION"] = javaVersion
+        val groovyJarFile = moduleRootManager.orderEntries().allLibrariesAndSdkClassesRoots.firstOrNull() {
             it.name.startsWith("groovy") && it.name.endsWith(".jar")
         }
-        val kotlinIncluded = moduleRootManager.orderEntries().allLibrariesAndSdkClassesRoots.any {
-            it.name.startsWith("kotlin-stdlib") && it.name.endsWith(".jar")
+        val kotlinJarFile = moduleRootManager.orderEntries().allLibrariesAndSdkClassesRoots.firstOrNull {
+            it.name.startsWith("kotlin") && it.name.endsWith(".jar")
         }
         var fileTemplateName = "JBang Java"
-        if (groovyIncluded) {
+        if (groovyJarFile != null) {
             fileTemplateName = "JBang Groovy"
-        } else if (kotlinIncluded) {
+            val groovyVersion = extractVersionFromJarFile(groovyJarFile.name) ?: "3.0.9"
+            properties["GROOVY_VERSION"] = groovyVersion;
+        } else if (kotlinJarFile != null) {
             fileTemplateName = "JBang Kotlin"
+            val kotlinVersion = extractVersionFromJarFile(kotlinJarFile.name) ?: "1.6.10"
+            properties["KOTLIN_VERSION"] = kotlinVersion;
         }
         val roots = moduleRootManager.sourceRoots
         if (roots.isNotEmpty()) {
             val srcRoot = roots[0]
+            val template = FileTemplateManager.getInstance(module.project).getInternalTemplate(fileTemplateName)
             ApplicationManager.getApplication().runWriteAction {
-                val template = FileTemplateManager.getInstance(module.project).getInternalTemplate(fileTemplateName)
-                val scriptFile = createFileFromTemplate("Hello", template, srcRoot.toPsiDirectory(module.project)!!, null, false)
-                if (scriptFile != null && (SystemInfo.isLinux || SystemInfo.isMac)) {
+                val scriptFile = FileTemplateUtil.createFromTemplate(
+                    template, "Hello",
+                    properties,
+                    srcRoot.toPsiDirectory(module.project)!!
+                ).containingFile
+                if (SystemInfo.isLinux || SystemInfo.isMac) {
                     File(scriptFile.virtualFile.path).setExecutable(true)
                 }
+                FileEditorManager.getInstance(module.project).openFile(scriptFile.virtualFile, true)
             }
         }
+    }
+
+    private fun extractVersionFromJarFile(jarFileName: String): String? {
+        val matcher = Pattern.compile(".*-(\\d+\\..*)\\.jar").matcher(jarFileName);
+        if (matcher.find()) {
+            return matcher.group(1)
+        }
+        return null
     }
 
 }
