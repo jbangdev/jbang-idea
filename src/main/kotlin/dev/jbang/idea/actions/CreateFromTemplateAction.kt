b3d9b9a6be0f99e27d11a9ef5f969a7ef6355083
linux_china <libing.chen@gmail.com>
2022-02-07 13:02:58 -0800

    Polishing

--- src/main/kotlin/dev/jbang/idea/actions/CreateFromTemplateAction.kt
+++ src/main/kotlin/dev/jbang/idea/actions/CreateFromTemplateAction.kt
@@ -8,7 +8,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.vfs.LocalFileSystem
-import dev.jbang.idea.JBangCli.generateScriptFrommTemplate
+import dev.jbang.idea.JBangCli.generateScriptFromTemplate
 import dev.jbang.idea.JBangCli.listJBangTemplates
 import org.jetbrains.kotlin.idea.caches.project.NotUnderContentRootModuleInfo.project
 
@@ -26,7 +26,7 @@ class CreateFromTemplateAction : AnAction(), DumbAware {
                         val directory = e.getData(CommonDataKeys.VIRTUAL_FILE)
                         try {
                             val destDir = directory?.path ?: project.basePath!!
-                            generateScriptFrommTemplate(templateName, scriptName, destDir)
+                            generateScriptFromTemplate(templateName, scriptName, destDir)
                             LocalFileSystem.getInstance().refresh(true)
                         } catch (e: Exception) {
                             val errorText = "Failed to create script from template, please check template and script name!"
