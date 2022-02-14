35e1e0ea83692a5f2f6ce3a40aab5ab1c063c2c7
linux_china <libing.chen@gmail.com>
2022-02-14 14:54:53 -0800

    Add GroovyLanguage construct param

--- src/main/kotlin/dev/jbang/idea/completion/directive/JBangGroovyCompletionContributor.kt
+++ src/main/kotlin/dev/jbang/idea/completion/directive/JBangGroovyCompletionContributor.kt
@@ -1,21 +1,18 @@
 package dev.jbang.idea.completion.directive
 
 import com.intellij.codeInsight.completion.CompletionParameters
-import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
-import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.patterns.PlatformPatterns
-import com.intellij.psi.PsiComment
-import com.intellij.psi.PsiFile
 import com.intellij.util.ProcessingContext
 import org.jetbrains.plugins.groovy.GroovyLanguage
 
-class JBangGroovyCompletionContributor : JBangBaseDirectiveCompletionContributor() {
-    override fun _addCompletions(parameters: CompletionParameters,
-                                context: ProcessingContext,
-                                result: CompletionResultSet) {
-        super._addCompletions(parameters, context, result)
+class JBangGroovyCompletionContributor : JBangBaseDirectiveCompletionContributor(GroovyLanguage) {
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
+        super.addCompletions(parameters, context, result)
         result.addElement(LookupElementBuilder.create("GROOVY").withTailText("Groovy version", true))
     }
 
