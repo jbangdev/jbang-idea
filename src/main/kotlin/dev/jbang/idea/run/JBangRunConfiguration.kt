c586e0a7a3fd962632dda1ec37d25246aa167a60
linux_china <libing.chen@gmail.com>
2022-05-08 17:31:31 -0700

    feat: add env variables support to run jbang

--- src/main/kotlin/dev/jbang/idea/run/JBangRunConfiguration.kt
+++ src/main/kotlin/dev/jbang/idea/run/JBangRunConfiguration.kt
@@ -11,6 +11,8 @@ import com.intellij.openapi.project.Project
 import dev.jbang.idea.getJBangCmdAbsolutionPath
 import dev.jbang.idea.jbangIcon
 import java.io.File
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 import javax.swing.Icon
 
 
@@ -50,6 +52,23 @@ class JBangRunConfiguration(
         options.setEnvVariables(envVariables)
     }
 
+    fun getEnvVariablesAsMap(): Map<String, String> {
+        val variables = getEnvVariables()
+        if (variables != null && variables.contains('=')) {
+            val variablesMap = mutableMapOf<String, String>()
+            //pairs like NAME=xxx or NAME="a b c d"
+            val p = Pattern.compile("(\\w+)=\"*((?<=\")[^\"]+(?=\")|(\\S+))\"*")
+            val m: Matcher = p.matcher(variables)
+            while (m.find()) {
+                val name = m.group(1)
+                val value = m.group(2)
+                variablesMap[name.uppercase()] = value
+            }
+            return variablesMap
+        }
+        return emptyMap()
+    }
+
     override fun getConfigurationEditor(): JBangRunSettingsEditor {
         return JBangRunSettingsEditor()
     }
@@ -87,6 +106,7 @@ class JBangRunConfiguration(
                 }
                 val commandLine = GeneralCommandLine(command)
                 commandLine.workDirectory = File(project.basePath!!)
+                commandLine.environment.putAll(getEnvVariablesAsMap())
                 val processHandler = ProcessHandlerFactory.getInstance().createColoredProcessHandler(commandLine) as ColoredProcessHandler
                 processHandler.setShouldKillProcessSoftly(true)
                 ProcessTerminatedListener.attach(processHandler)
