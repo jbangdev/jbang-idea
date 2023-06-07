56bfbf452de111adb306b6e02f51fa1e243bc85a
Libing Chen <libing.chen@gmail.com>
2023-06-08 00:24:56 +0800

    feat: add //PREVIEW support

--- src/main/kotlin/dev/jbang/idea/completion/directive/JBangJavaCompletionContributor.kt
+++ src/main/kotlin/dev/jbang/idea/completion/directive/JBangJavaCompletionContributor.kt
@@ -1,9 +1,21 @@
 package dev.jbang.idea.completion.directive
 
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.lang.java.JavaLanguage
+import com.intellij.util.ProcessingContext
 
 
 class JBangJavaCompletionContributor : JBangBaseDirectiveCompletionContributor(JavaLanguage.INSTANCE) {
 
+    override fun addCompletions(
+            parameters: CompletionParameters,
+            context: ProcessingContext,
+            result: CompletionResultSet
+    ) {
+        super.addCompletions(parameters, context, result)
+        result.addElement(LookupElementBuilder.create("PREVIEW").withTailText(" --enable-preview flag", true))
+    }
 
 }  
