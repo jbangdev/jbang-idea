35e347fcf504f274cc4f9da7746e5dd805684799
Libing Chen <libing.chen@gmail.com>
2024-07-08 23:15:42 +0800

    build: update to Gradle IntelliJ Plugin 1.17.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,9 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.9.23"
+    id("org.jetbrains.kotlin.jvm") version "1.9.24"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.17.2"
+    id("org.jetbrains.intellij") version "1.17.4"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
